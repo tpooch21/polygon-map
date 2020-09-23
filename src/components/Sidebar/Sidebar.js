48dc2047372b7d3c938a4259d70248e43a68b897
Trevor Puccini <trevorpuccini21@gmail.com>
2020-09-22 19:37:30 -0700

    Refactor SideBar to use Chakra for styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-import classes from './Sidebar.module.css';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Icon,
+  Text,
+  Flex,
+
+} from "@chakra-ui/core";
 
 const Sidebar = props => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
   return (
-    <div className={classes.Sidebar}>
-        <h2>Highlight a Region on the Map</h2>
-        <p>Use the toggle button below to show/hide a polygon covering the specified area</p>
-        <button onClick={props.togglePolygon}>Toggle Polygon</button>
-    </div>
+    <Flex>
+      <Button
+        ref={btnRef}
+        m={2}
+        onClick={onOpen}
+        cursor="pointer"
+        color="black"
+        w={32}
+        h={8}>
+        <Icon
+          name="arrow-right"
+          mr={2}/>
+        <Text fontSize="sm">Show Sidebar</Text>
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent >
+          <DrawerCloseButton
+            w={6}
+            h={6}
+            cursor="pointer"/>
+          <DrawerHeader size="m">View a Region of the Map</DrawerHeader>
+          <DrawerBody>
+            <Text>Click on the button below to show/hide the polygon displayed on the map</Text>
+            <Button
+              onClick={props.togglePolygon}
+              cursor="pointer"
+              variantColor="blue"
+              w={32}
+              h={8}
+              fontSize="sm">Toggle Polygon</Button>
+          </DrawerBody>
+          <DrawerFooter>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </Flex>
   );
 };
 
