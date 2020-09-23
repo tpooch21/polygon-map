a150f434cef4aa321db20cfbd087461349f016fd
Trevor Puccini <trevorpuccini21@gmail.com>
2020-09-22 17:53:54 -0700

    Create state and updateState handlers in App component to handle toggling of polygon and sidebar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 
 import Map from './components/Map/Map';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   const [showSidebar, toggleSidebarHandler] = useState(true);
+  const [displayPolygon, togglePolygonHandler] = useState(true);
 
   return (
     <div className="App">
       <main className="App-main">
-        <Map />
-        {/* Sidebar */}
+        <Map showPolygon={displayPolygon}/>
+        <Sidebar
+          togglePolygon={() => togglePolygonHandler(!displayPolygon)}
+          toggleSidebar={() => toggleSidebarHandler(!showSidebar)}/>
       </main>
     </div>
   );
